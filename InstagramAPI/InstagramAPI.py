0bc0da4cc69476c2e4c7ddb92eeef3258b349088
Drew Harris <=>
2018-04-24 19:52:34 -0500

    added proxy print

--- InstagramAPI/InstagramAPI.py
+++ InstagramAPI/InstagramAPI.py
@@ -82,6 +82,7 @@ class InstagramAPI:
         if proxy is not None:
             print('Set proxy!')
             proxies = {'http': proxy, 'https': proxy}
+            print(proxies)
             self.s.proxies.update(proxies)
 
     def login(self, force=False):
